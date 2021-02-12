3919c3c1010c03b97b40799bc9d9637dc2b0118a
paramthakker <37357102+paramthakker@users.noreply.github.com>
2021-02-11 23:59:32 -0500

    Sorted songs in ascending order

--- src/view/SongViewController.java
+++ src/view/SongViewController.java
@@ -104,8 +104,6 @@ public class SongViewController {
 	}
 	class sortSongs implements Comparator<String> 
 	{ 
-	    // Used for sorting in ascending order of 
-	    // roll number 
 	    public int compare(String a, String b) 
 	    { 
 	       String[] aDivide=a.split("|",2);
