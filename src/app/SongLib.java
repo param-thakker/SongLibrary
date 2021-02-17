7d3e3a79c4cdcc5b671650ee4b18b64509c329ad
Jonathan Lu <jnl76@scarletmail.rutgers.edu>
2021-02-16 20:21:15 -0500

    SplitPane now works properly displaying song deets

--- src/app/SongLib.java
+++ src/app/SongLib.java
@@ -2,36 +2,36 @@ package app;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.scene.control.SplitPane;
 import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import view.SongViewController;
 
 public class SongLib extends Application {
 
-
 	@Override
 	public void start(Stage primaryStage) throws Exception {
+		// TODO Auto-generated method stub
 		FXMLLoader loader = new FXMLLoader();   
 		loader.setLocation(
-				getClass().getResource("/view/songlist.fxml"));
+				getClass().getResource("/view/songlist2.fxml"));
 		AnchorPane root = (AnchorPane)loader.load();
 
 		SongViewController listController = 
 				loader.getController();
 		listController.start(primaryStage);
 
-		Scene scene = new Scene(root, 400, 300);
+		Scene scene = new Scene(root, 600, 400);
 		primaryStage.setScene(scene);
+		primaryStage.setTitle("Song Library");
 		primaryStage.show(); 
 
 	}
 
 	public static void main(String[] args) {
+		// TODO Auto-generated method stub
 		launch(args);
 
 	}
 
 }
+
